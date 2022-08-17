4b3798c6af1915f4e1d447ab311f003ad25991d0
Colin Lennon <colin.lennon@live.com>
2022-08-17 21:20:46 +0100

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-if [ ${{ github.event.inputs.secret-1 }} -ne "" ]
+SECRET_1=""
+SECRET_2=""
+SECRET_3=""
+SECRET_4=""
+SECRET_5=""
+if [ $1 -ne "" ]
+then
+  SECRET_1=$(java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
+  string \
+  encrypt \
+  AES \
+  CBC \
+  ${{ secrets.ENCRYPTION_KEY }} \
+  ${{ github.event.inputs.secret-1 }})
+fi
+if [ $2 -ne "" ]
 then
   java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
   string \
@@ -9,3 +24,39 @@ then
   ${{ secrets.ENCRYPTION_KEY }} \
   ${{ github.event.inputs.secret-1 }}
 fi
+if [ $3 -ne "" ]
+then
+  java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
+  string \
+  encrypt \
+  AES \
+  CBC \
+  ${{ secrets.ENCRYPTION_KEY }} \
+  ${{ github.event.inputs.secret-1 }}
+fi
+if [ $4 -ne "" ]
+then
+  java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
+  string \
+  encrypt \
+  AES \
+  CBC \
+  ${{ secrets.ENCRYPTION_KEY }} \
+  ${{ github.event.inputs.secret-1 }}
+fi
+if [ $5 -ne "" ]
+then
+  java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
+  string \
+  encrypt \
+  AES \
+  CBC \
+  ${{ secrets.ENCRYPTION_KEY }} \
+  ${{ github.event.inputs.secret-1 }}
+fi
+
+echo "Secret 1: $SECRET_1"
+echo "Secret 2: $SECRET_1"
+echo "Secret 3: $SECRET_1"
+echo "Secret 4: $SECRET_1"
+echo "Secret 5: $SECRET_1"
