c032245b7d0292c946a576e0b01181cb68f20002
Colin Lennon <colin.lennon@live.com>
2022-08-17 21:38:05 +0100

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -11,47 +11,23 @@ then
 fi
 if [ $3 != "" ]
 then
-  java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
-  string \
-  encrypt \
-  AES \
-  CBC \
-  ${{ secrets.ENCRYPTION_KEY }} \
-  $3
+  SECRET_2=$(java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool string encrypt AES CBC $ENCRYPTION_KEY $3)
 fi
 if [ $4 != "" ]
 then
-  java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
-  string \
-  encrypt \
-  AES \
-  CBC \
-  ${{ secrets.ENCRYPTION_KEY }} \
-  $4
+  SECRET_3=$(java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool string encrypt AES CBC $ENCRYPTION_KEY $4)
 fi
 if [ $5 != "" ]
 then
-  java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
-  string \
-  encrypt \
-  AES \
-  CBC \
-  ${{ secrets.ENCRYPTION_KEY }} \
-  $5
+  SECRET_4=$(java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool string encrypt AES CBC $ENCRYPTION_KEY $5)
 fi
 if [ $6 != "" ]
 then
-  java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool \
-  string \
-  encrypt \
-  AES \
-  CBC \
-  ${{ secrets.ENCRYPTION_KEY }} \
-  $6
+  SECRET_5=$(java -cp secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool string encrypt AES CBC $ENCRYPTION_KEY $6)
 fi
 
 echo "Secret 1: $SECRET_1"
-echo "Secret 2: $SECRET_1"
-echo "Secret 3: $SECRET_1"
-echo "Secret 4: $SECRET_1"
-echo "Secret 5: $SECRET_1"
+echo "Secret 2: $SECRET_2"
+echo "Secret 3: $SECRET_3"
+echo "Secret 4: $SECRET_4"
+echo "Secret 5: $SECRET_5"
